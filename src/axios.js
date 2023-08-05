7bc5dc3ef33265e05f249195422c7c2a1b8e2f79
ruchbiahadian <87080676+ruchbiahadian@users.noreply.github.com>
2023-08-05 17:39:17 +0700

    setting base url

--- src/axios.js
+++ src/axios.js
@@ -8,6 +8,6 @@ import axios from "axios";
 
 
 export const makeRequest = axios.create({
-    baseURL:"amre-server-new-production.up.railway.app",
+    baseURL:"amre-server-new-production.up.railway.app/api/",
     withCredentials: true,
 });
