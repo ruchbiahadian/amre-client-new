198eb0c927f52665c50e8863eee8e1cf214b30b0
ruchbiahadian <87080676+ruchbiahadian@users.noreply.github.com>
2023-08-05 18:24:42 +0700

    setting origin

--- src/axios.js
+++ src/axios.js
@@ -8,6 +8,6 @@ import axios from "axios";
 
 
 export const makeRequest = axios.create({
-    baseURL:"https://amre-server-new-production.up.railway.app",
+    baseURL:"https://amre-server-new-production.up.railway.app/api/",
     withCredentials: true,
 });
