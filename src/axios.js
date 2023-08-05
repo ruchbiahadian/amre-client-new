0deb763fe9c80ad2380f15d16931b76bd1a4c017
ruchbiahadian <87080676+ruchbiahadian@users.noreply.github.com>
2023-08-05 17:19:53 +0700

    finish

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,13 @@
 import axios from "axios";
 
+// export const makeRequest = axios.create({
+//     baseURL:"http://localhost:8800/api/",
+//     withCredentials: true,
+// });
+
+
+
 export const makeRequest = axios.create({
-    baseURL:"http://localhost:8800/api/",
+    baseURL:"amre-server-new-production.up.railway.app",
     withCredentials: true,
 });
