7a22377f4e43f0996eadb8b7600e0916a8471af4
ruchbiahadian <87080676+ruchbiahadian@users.noreply.github.com>
2023-08-06 15:40:27 +0700

    img handling

--- src/components/update/Update.jsx
+++ src/components/update/Update.jsx
@@ -28,7 +28,6 @@ const Update = ({setOpenUpdate, user}) =>{
     }, {
         onSuccess: () =>{
             queryClient.invalidateQueries(["user"])
-            window.location.reload();
         },
     })
 
